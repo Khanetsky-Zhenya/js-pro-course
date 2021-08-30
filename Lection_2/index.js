e70089cb5d6c0d040f4487cc012fe59dcbb117c6
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2021-08-30 11:55:53 +0300

    add:add 45% solution task #3

--- Lection_2/index.js
+++ Lection_2/index.js
@@ -27,6 +27,24 @@ promise
 /* Задача 3
 Сделайте 3 промиса, в каждом из которых расположена функция setTimeout со случайной задержкой от 1 до 5 секунд. Пусть каждый промис своим результатом возвращает эту задержку. С помощью Promise.all получите массив результатов, найдите его сумму, выведите на экран.*/
 
+
+function getRandonTimeOut(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+let randonTimeOut = getRandonTimeOut(1, 6)
+console.log(randonTimeOut)
+
+let array = [
+new Promise((resolve, reject) => {
+    setTimeout(() => resolve(randonTimeOut), randonTimeOut*1000);
+}),
+new Promise((resolve, reject) => {
+    setTimeout(() => resolve(randonTimeOut), randonTimeOut*1000);
+}),
+new Promise((resolve, reject) => {
+    setTimeout(() => resolve(randonTimeOut), randonTimeOut*1000);
+})
+]
 //=================================================================================================================
 
 /* Задача 4
