09a00ae6f8000c11969de98084d74722eb564cbe
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2021-09-05 20:00:35 +0300

    add:add solution task #2

--- Lection_3/index.js
+++ Lection_3/index.js
@@ -25,3 +25,53 @@ a.dicrement()
 a.returnCount()
 
 //========================================================================================================
+/*Задача 2
+Реализуйте класс Worker (Работник), который будет создавать объект и иметь следующие свойства:
+name (имя), secondName (фамилия), rate (ставка за день работы), days (количество отработанных дней)
+сountOfDetails(количество выполненных деталей). Также класс должен иметь метод getSalary(),
+который будет выводить зарплату работника. Зарплата - это произведение (умножение) ставки rate на количество
+отработанных дней days. У каждого объекта класса Worker должны быть доступны методы getName, getSecondName, getRate,
+getDays, каждый из которых будет возвращать соответствующее поле в объекте. И также методы setRate, setDays,
+которые будут устанавливать новые значения для соответственно свойств rate и days. Добавить метод, который будет
+увеличивать количество деталей на 1. Добавить метод, который будет делать ресет этого количества в 0
+*/
+
+class Worker {
+    constructor(name, secondName, rate, days, сountOfDetails){
+        this.name = name;
+        this.secondName = secondName;
+        this.rate = rate;
+        this.days = days;
+        this.сountOfDetails = сountOfDetails;
+    }
+    getSalary(){
+        return this.rate * this.days
+    }
+    getName(){
+        return this.name
+    }
+    getSecondName(){
+        return this.secondName
+    }
+    getRate(){
+        return this.rate
+    }
+    getDays(){
+        return this.days
+    }
+    setRate(newRate){
+        this.rate = newRate;
+    }
+    setDays(newDays){
+        this.days = newDays;
+    }
+    incrementDetails(){
+        this.сountOfDetails++;
+    }
+    resetCountOfDetails(){
+        this.сountOfDetails = 0
+    }
+}
+let workerFirst = new Worker('Ivan', 'Ivanov', 150, 3, 600)
+
+//========================================================================================================
